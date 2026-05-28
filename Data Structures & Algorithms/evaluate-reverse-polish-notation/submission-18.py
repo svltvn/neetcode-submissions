b7dc934373aa365b28ce2930fb09ba33e2bed940
Sultan <tahirsaif100@gmail.com>
2026-05-28 08:53:18 -0400

    Add: evaluate-reverse-polish-notation - submission-18

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-18.py
@@ -0,0 +1,27 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        '''
+        Second attempt here, will see if I can build out constraints, then BF, then find optimal solution
+        '''
+        op = ["+", "-", "*", "/"]
+        stack = []
+        for c in tokens:
+            print(stack)
+            if c in op:
+                print(c)
+                x2 = stack.pop()
+                x1 = stack.pop()
+                if c == "+":
+                    stack.append(x1+x2)
+                elif c == "-":
+                    stack.append(x1-x2)
+                elif c == "*":
+                    stack.append(x1*x2)
+                elif c == "/":
+                    stack.append(int(x1/x2))
+            else:
+                stack.append(int(c))
+            
+        return stack[0]
+
+                
